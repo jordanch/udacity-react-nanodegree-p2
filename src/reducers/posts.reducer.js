23b461727eee20f065d9c12b14f84a5cdb9ef969
Jordan Cole Hunt <jordancolehunt@gmail.com>
2018-03-03 20:58:41 +0200

    remove dead coce.

--- src/reducers/posts.reducer.js
+++ src/reducers/posts.reducer.js
@@ -3,35 +3,6 @@ import {
   RECEIVE_ALL_POSTS,
   RECEIVE_POST_DETAIL
 } from "../actions/posts.actions";
-// const initialPostsState = {
-//   byId: {
-//     "8xf0y6ziyjabvozdd253nd": {
-//       id: "8xf0y6ziyjabvozdd253nd",
-//       timestamp: 1467166872634,
-//       title: "Udacity is the best place to learn React",
-//       body: "Everyone says so after all.",
-//       author: "thingtwo",
-//       category: "react",
-//       voteScore: 6,
-//       deleted: false,
-//       commentCount: 2,
-//       commentIds: []
-//     },
-//     "6ni6ok3ym7mf1p33lnez": {
-//       id: "6ni6ok3ym7mf1p33lnez",
-//       timestamp: 1468479767190,
-//       title: "Learn Redux in 10 minutes!",
-//       body: "Just kidding. It takes more than 10 minutes to learn technology.",
-//       author: "thingone",
-//       category: "redux",
-//       voteScore: -5,
-//       deleted: false,
-//       commentCount: 0,
-//       commentIds: ["8tu4bsun805n8un48ve89"]
-//     }
-//   },
-//   allIds: ["post1", "post2"]
-// };
 
 const initialPostsState = {
   byId: {},
@@ -61,7 +32,6 @@ export default function posts(state = initialPostsState, action) {
         state.byId[post.id] && state.byId[post.id].commentIds
           ? state.byId[post.id].commentIds
           : [];
-      // const orchestratedPost = orchestratePostDetailResponse(action.post);
       return Object.assign({}, state, {
         byId: {
           ...state.byId,
